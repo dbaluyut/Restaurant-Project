4b6a7583e69bd7f882f16c7d6c1c1be49fa3e26b
Jesse Harvey <jesselharvey@gmail.com>
2020-09-17 15:50:32 -0700

    initial body structure

---
+++ main-jh.js
@@ -0,0 +1,45 @@
+// API order
+// {apps: [
+//    item {
+//        name: etc.
+//        desc: yes,no
+//        extra: {
+//          spicy
+//            }
+//      }      
+//    ]}
+// {entrees: [
+//    item {
+//        name: etc.
+//        desc: yes,no
+//        extra: {
+//          spicy
+//            }
+//      }      
+//    ]}
+// {desserts: [
+//    item {
+//        name: etc.
+//        desc: yes,no
+//        extra: {
+//          spicy
+//            }
+//      }      
+//    ]}
+
+//......................................................
+//.MMMMM...MMMMM..EEEEEEEEEEE.NNNN....NNN..UUUU...UUUU..
+//.MMMMM...MMMMM..EEEEEEEEEEE.NNNNN...NNN..UUUU...UUUU..
+//.MMMMMM..MMMMM..EEEEEEEEEEE.NNNNN...NNN..UUUU...UUUU..
+//.MMMMMM.MMMMMM..EEEE........NNNNNN..NNN..UUUU...UUUU..
+//.MMMMMM.MMMMMM..EEEEEEEEEE..NNNNNNN.NNN..UUUU...UUUU..
+//.MMMMMM.MMMMMM..EEEEEEEEEE..NNNNNNN.NNN..UUUU...UUUU..
+//.MMMMMM.MMMMMM..EEEEEEEEEE..NNN.NNNNNNN..UUUU...UUUU..
+//.MMMMMMMMMMMMM..EEEEEEEEEE..NNN.NNNNNNN..UUUU...UUUU..
+//.MMM.MMMMMMMMM..EEEE........NNN..NNNNNN..UUUU...UUUU..
+//.MMM.MMMMM.MMM..EEEE........NNN..NNNNNN..UUUU...UUUU..
+//.MMM.MMMMM.MMM..EEEEEEEEEEE.NNN...NNNNN..UUUUUUUUUUU..
+//.MMM.MMMMM.MMM..EEEEEEEEEEE.NNN...NNNNN...UUUUUUUUU...
+//.MMM.MMMMM.MMM..EEEEEEEEEEE.NNN....NNNN....UUUUUUU....
+//......................................................
+//......................................................
