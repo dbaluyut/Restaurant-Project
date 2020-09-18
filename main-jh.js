d3737524307194a29e7e82699d2db3f6756b0861
Jesse Harvey <jesselharvey@gmail.com>
2020-09-18 16:10:54 -0700

    add appetizer sect.

--- main-jh.js
+++ main-jh.js
@@ -51,16 +51,61 @@ const desserts = document.querySelector("#desserts")
 fetch("https://obscure-tundra-54269.herokuapp.com/casual-dining")
   .then((resp) => resp.json())
   .then((menu) => {
-    function appFunct() {
-      for (let i = 0; i < menu.appetizers.length; i++) {
-        console.log(menu.appetizers[i])
-        console.log(menu.appetizers[i].name)
-        return `<div class="itemLine">
-        <h3>${menu.appetizers[i].name}</h3>
-        </div>`
-        // console.log(menu)
-        // console.log(menu.appetizers)
-      }
+    return menu.appetizers
+  })
+  .then((item) => {
+    appFunct = () => {
+      console.log(item)
+      // console.log(item.name)
+      // for (let x of item) {
+      //   // console.log(x)
+      //   console.log(item[x])
+      //   console.log(item[x].name)
+      //   return `<h3>${item[x].name}</h3>`
+      // }
+      return item
+        .map((x) => {
+          console.log(x)
+          console.log(x.name)
+          console.log(x.price)
+          return `<div class="itemLine">
+          <h3 class="itemName">${x.name}</h3>
+          <div class="vertBreak"></div>
+          <h5>${x.price}</h5>
+          </div>
+          <div class="descLine">
+          <span>${x.description}</span>
+          <div class="foodInfo"></div>
+          </div>`
+        })
+        .join("")
+    .then((menu) => {
+      return menu.entrees
+    })
+    .then((item) => {
+      entreeFunct
+    })
+      //for the food info, should I use an if statement?
+      // or like filter it orrrr what my guy?
+
+      // console.log(item)
+      // console.log(item.name)
+      // for (let x in item) {
+      //   console.log(item)
+      //   // console.log(menu.[x])
+      //   // console.log(menu.[x].name)
+      //   // return `<div class="itemLine">
+      //   // <h3>${item[x].name}</h3>
+      //   // </div>`
+      // }
+      // for (let i = 0; i < menu.appetizers.length; i++) {}
+      //   console.log(menu.appetizers[i])
+      //   console.log(menu.appetizers[i].name)
+      // return `<div class="itemLine">
+      // <h3>${menu.appetizers[i].name}</h3>
+      // </div>`
+      // console.log(menu)
+      // console.log(menu.appetizers)
     }
     // console.log(menu.appetizers[0].name)
     // for (let i = 0; i < menu.appetizers.length; i++) {}
